5fc68c7d0ac720d63b2de2df9781fcfdb43a67b7
Abhishek2096 <abhishek20gupta96@gmail.com>
2023-05-21 12:43:22 -0400

    Static Pages changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,23 +3,27 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = 'djangoapp' 
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
 
-    # path for about view
-
-    # path for contact us view
-
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    #path('registration/', views.registration_request, name='registration'),
+    #path('login/', views.login_request, name='login'),
+    #path('logout/', views.logout_request, name='logout'),
+    #path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    #path('dealer/<int:id>/review', views.add_review, name='add_review'),
+    path(route='', view=views.get_dealerships, name='index')
     # path for registration
 
     # path for login
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for dealer reviews view
 
